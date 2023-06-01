6f2da83fd74a4735ed826ec3f47761905b8901f3
Julian Villanueva <andres07021@hotmail.com>
2023-06-01 14:47:33 -0500

    Start project

---
+++ src/main/java/com/project/coches/CochesYtApplication.java
@@ -0,0 +1,13 @@
+package com.project.coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochesYtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CochesYtApplication.class, args);
+    }
+
+}
